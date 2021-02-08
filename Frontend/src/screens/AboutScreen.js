e208575228d6a65e889346b4d78d37b971a1e91a
AffirmedVisionary <iam@charlenegrant.co.uk>
2021-02-08 17:17:03 +0000

    changed image width

--- Frontend/src/screens/AboutScreen.js
+++ Frontend/src/screens/AboutScreen.js
@@ -5,7 +5,7 @@ const AboutScreen = () => {
         <>
             <h1>About</h1>
             <p>Welcome to the about page</p>
-            <img src='https://cdn.pixabay.com/photo/2018/05/12/03/00/shy-3392329_1280.jpg' alt='test girl at sea' />
+            <img src='https://cdn.pixabay.com/photo/2018/05/12/03/00/shy-3392329_1280.jpg' alt='test girl at sea' width='80%' height='auto' />
         </>
     )
 }
--- Frontend/src/screens/BdcvScreen.js
+++ Frontend/src/screens/BdcvScreen.js
@@ -5,7 +5,7 @@ const BdcvScreen = () => {
         <>
             <h1>Bdcv</h1>
             <p>Welcome to the hub of Charlene Grant</p>
-            <img src='https://cdn.pixabay.com/photo/2018/05/12/03/00/shy-3392329_1280.jpg' alt='test girl at sea' />
+            <img src='https://cdn.pixabay.com/photo/2018/05/12/03/00/shy-3392329_1280.jpg' alt='test girl at sea' width='80%' height='auto' />
         </>
     )
 }
