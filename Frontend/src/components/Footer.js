abcdf64bf1ad048718cce4d1015036fe1a0633d8
AffirmedVisionary <iam@charlenegrant.co.uk>
2021-02-08 17:08:25 +0000

    Added Footer Component

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Socials from './Socials'
+import "../App.css"
 
 const Footer = () => {
     return (
-        <div>
+        <div id='footer'>
+            <hr className='foot-line'></hr>
             <span>Thank you for visiting</span>
+            <Socials />
+            <hr className='foot-line'></hr>
+            <span>:Copyright/Copy Claim :charlene :grant</span>
         </div>
     )
 }
