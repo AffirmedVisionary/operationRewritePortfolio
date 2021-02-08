c840fe8676ce8d52164253e6b1b63f279588b032
AffirmedVisionary <iam@charlenegrant.co.uk>
2021-02-08 17:07:56 +0000

    edited screen layout

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 // import items from "./screens/Sidebar-Items"
 import { BrowserRouter as Router, Route } from "react-router-dom"
-import { Container } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import HomeScreen from "./screens/HomeScreen"
 import AboutScreen from "./screens/AboutScreen"
 import "./App.css"
@@ -22,19 +22,18 @@ function App() {
 
 
   return (
-    <>
-                    <Router>
-        <div className='App'>
-          <div id='navspace'>Top Menu</div>
-          <div className='row'>
-         
-            <div className='col' id='side-col'>
-                            <Sidebar3 />
-            
-              </div>
-            <div className='col' id='main-col'>
-                <Container id='screenContainer'>
-              <Route path='/' component={HomeScreen} exact />
+    <React.Fragment id='page'>
+      <Router>
+        <Row id='App'>
+          <Row id='top-menu'>
+            Top Menu on mobile screens
+          </Row>
+          <Row>
+            <Col id='side-col' lg={2}>
+            <Sidebar3 />
+            </Col>
+            <Col id='main-col'>
+            <Route path='/' component={HomeScreen} exact />
               <Route path='/about' component={AboutScreen} exact />
               <Route path='/resume' component={ResumeScreen} exact />
               <Route path='/bdcv' component={BdcvScreen} exact />
@@ -42,47 +41,12 @@ function App() {
               <Route path='/profile' component={ProfileScreen} exact />
               <Route path='/content' component={ContentScreen} exact />
               <Route path='/contact' component={ContactScreen} exact />
-              <div className='footer'>
-                <Footer />
-                </div>
-                </Container>
-            </div>
-          {/*         <div className="col">
-          <div className="box">Col 3/3</div>
-        </div>
- */}
-        </div>
-        </div>
+              <Footer />
+            </Col>
+          </Row>
+        </Row>
         </Router>
-    </>
-    /*     <Router>
-<Grid columns='equal'>
-    <Grid.Column>
-      <Segment>      <Sidebar items={items} /> </Segment>
-    </Grid.Column>
-    <Grid.Column width={8}>
-          <Segment>
-          <Route path='/' component={HomeScreen} exact />
-
-      </Segment>
-    </Grid.Column>
-    <Grid.Column>
-      <Segment>3</Segment>
-    </Grid.Column>
-  </Grid>
-    </Router> */
-    /*     <Router>
-      <Sidebar items={items}
-      <main className='py-4'>
-
-           <Container fluid>
-
-            <Route path='/' component={HomeScreen} exact />
-            </Container>
-
-        </main>
-    </Router>
- */
+    </React.Fragment>
   )
 }
 
